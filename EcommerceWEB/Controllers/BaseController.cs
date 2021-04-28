8aae57dcba7d2fd938eb56cda60023660078ec55
Kadin Nguyen <kadinxuantruong@gmail.com>
2021-04-28 23:35:37 +0700

    Add ecommerceweb .net core

---
+++ EcommerceWEB/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceAPI.DataAccess.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWEB.Controllers
+{
+    public class BaseController : Controller
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public BaseController()
+        {
+
+        }
+        public BaseController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+    }
+}
---
+++ EcommerceWEB/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EcommerceAPI.DataAccess.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceWEB.Controllers
+{
+    public class UserController : BaseController
+    {   protected readonly IUnitOfWork _unitofwork;
+        public UserController(IUnitOfWork _unitofwork) : base(_unitofwork)
+        {
+        }
+        public IActionResult Index()
+        {
+            _unitOfWork.UserResponsitory.GetAll();
+            return View();
+        }
+    }
+}
