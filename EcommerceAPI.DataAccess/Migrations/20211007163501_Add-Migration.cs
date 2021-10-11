f3a9a07ee152a226a79f744585ee4852c75afe4f
xuantruong1999 <kadinxuantruong1999@gmail.com>
2021-10-11 23:28:25 +0700

    Implement refresh token feature

---
+++ EcommerceAPI.DataAccess/Migrations/20211007163501_Add-Migration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EcommerceAPI.DataAccess.Migrations
+{
+    public partial class AddMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenTimeStamp",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenTimeStamp",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ EcommerceAPI.Model/User/UserRegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EcommerceAPI.Model.User
+{
+    public class UserRegisterModel
+    {
+        [Required, MinLength(6), MaxLength(14)]
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+    }
+}
